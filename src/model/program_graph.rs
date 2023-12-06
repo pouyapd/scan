3459301fa6b76f135c81360de53d4ba134045fca
EnricoGhiorzi <58953206+EnricoGhiorzi@users.noreply.github.com>
2023-12-06 18:54:04 +0100

    Improved program graphs internal representation and interface (further)

--- src/model/program_graph.rs
+++ src/model/program_graph.rs
@@ -1,9 +1,4 @@
-//! TODO list:
-//! -[ ] use fast hasher for hashmap and hashset
-
-use std::{collections::HashMap, rc::Rc};
-
-use super::formula::*;
+use std::{collections::HashMap, error::Error, fmt, rc::Rc};
 
 // Use of "Newtype" pattern to define different types of indexes.
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -13,43 +8,142 @@ pub struct Location(usize);
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct Action(usize);
 
-type Effect = Box<dyn Fn(&mut Eval)>;
+// Use of "Newtype" pattern to define different types of indexes.
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Var(usize);
 
-type Transitions = Vec<HashMap<(Action, Location), Formula>>;
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub enum VarType {
+    Boolean,
+    Integer,
+}
 
-#[derive(Debug)]
-pub enum PgErr {
+pub type Integer = i32;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub enum Val {
+    Boolean(bool),
+    Integer(Integer),
+}
+
+#[derive(Debug, Clone)]
+pub enum IntExpr {
+    Const(Integer),
+    Var(Var),
+    Opposite(Box<IntExpr>),
+    Sum(Box<IntExpr>, Box<IntExpr>),
+    Mult(Box<IntExpr>, Box<IntExpr>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Formula {
+    And(Box<Formula>, Box<Formula>),
+    Or(Box<Formula>, Box<Formula>),
+    Implies(Box<Formula>, Box<Formula>),
+    Not(Box<Formula>),
+    Prop(Var),
+    Equal(IntExpr, IntExpr),
+    Less(IntExpr, IntExpr),
+    LessEq(IntExpr, IntExpr),
+    True,
+    False,
+}
+
+#[derive(Debug, Clone)]
+pub enum Effect {
+    Bool(Formula),
+    Integer(IntExpr),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PgError {
     MissingAction(Action),
     MissingLocation(Location),
+    Mismatched,
+    NonExistingVar(Var),
+    NoTransition,
+    UnsatisfiedGuard,
 }
 
-pub struct ProgramGraph {
-    // `Hashmap`s (and thus `Eval`) is not hashable,
-    // so we need to use a lambda to represent the inner function type.
-    effects: Vec<Effect>,
+impl fmt::Display for PgError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PgError::MissingAction(action) => write!(
+                f,
+                "action {:?} does not belong to this program graph",
+                action
+            ),
+            PgError::MissingLocation(location) => write!(
+                f,
+                "location {:?} does not belong to this program graph",
+                location
+            ),
+            PgError::Mismatched => write!(f, "type mismatch"),
+            PgError::NonExistingVar(var) => {
+                write!(f, "var {:?} does not belong to this program graph", var)
+            }
+            PgError::NoTransition => write!(f, "There is no such transition"),
+            PgError::UnsatisfiedGuard => write!(f, "The guard has not been satisfied"),
+        }
+    }
+}
+
+impl Error for PgError {}
+
+pub struct ProgramGraphBuilder {
+    // Effects are indexed by actions
+    effects: Vec<Vec<(Var, Effect)>>,
+    // Transitions are indexed by locations
     // We can assume there is at most one condition by logical disjunction
-    transitions: Transitions,
+    transitions: Vec<HashMap<(Action, Location), Formula>>,
+    vars: Vec<VarType>,
 }
 
-impl Default for ProgramGraph {
+impl Default for ProgramGraphBuilder {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl ProgramGraph {
+impl ProgramGraphBuilder {
+    const INITIAL_LOCATION: Location = Location(0);
+
     pub fn new() -> Self {
-        Self {
+        let mut pgb = Self {
             effects: Vec::new(),
             transitions: Vec::new(),
-        }
+            vars: Vec::new(),
+        };
+        // Create an initial location and make sure it is equal to the constant `Self::INITIAL_LOCATION`
+        // This is the simplest way to make sure the state of the builder is always consistent
+        let initial_location = pgb.new_location();
+        assert_eq!(initial_location, Self::INITIAL_LOCATION);
+        pgb
     }
 
-    pub fn new_action(&mut self, effect: impl Fn(&mut Eval) + 'static) -> Action {
+    pub fn new_var(&mut self, var_type: VarType) -> Var {
+        let idx = self.vars.len();
+        self.vars.push(var_type);
+        Var(idx)
+    }
+
+    pub fn new_action(&mut self) -> Action {
         // Actions are indexed progressively
         let idx = self.effects.len();
-        self.effects.push(Box::new(effect));
+        self.effects.push(Vec::new());
         Action(idx)
+    }
+
+    pub fn add_effect(&mut self, action: Action, var: Var, effect: Effect) -> Result<(), PgError> {
+        match self.vars.get(var.0).ok_or(PgError::NonExistingVar(var))? {
+            VarType::Boolean if !matches!(effect, Effect::Bool(_)) => Err(PgError::Mismatched),
+            VarType::Integer if !matches!(effect, Effect::Integer(_)) => Err(PgError::Mismatched),
+            _ => self
+                .effects
+                .get_mut(action.0)
+                .ok_or(PgError::MissingAction(action))
+                .map(|effects| effects.push((var, effect))),
+        }
     }
 
     pub fn new_location(&mut self) -> Location {
@@ -65,11 +159,23 @@ impl ProgramGraph {
         action: Action,
         post: Location,
         guard: Formula,
-    ) -> Result<(), PgErr> {
+    ) -> Result<(), PgError> {
+        self.typecheck(&guard)?;
+        // Check 'pre' and 'post' locations exists
+        if self.transitions.len() <= pre.0 {
+            return Err(PgError::MissingLocation(pre));
+        }
+        if self.transitions.len() <= post.0 {
+            return Err(PgError::MissingLocation(post));
+        }
+        // Check 'action' exists
+        if self.effects.len() <= action.0 {
+            return Err(PgError::MissingAction(action));
+        }
         let _ = self
             .transitions
             .get_mut(pre.0)
-            .ok_or(PgErr::MissingLocation(pre))?
+            .expect("location existance already checked")
             .entry((action, post))
             .and_modify(|previous_guard| {
                 *previous_guard =
@@ -78,64 +184,260 @@ impl ProgramGraph {
             .or_insert(guard);
         Ok(())
     }
-}
 
-#[derive(Debug)]
-pub enum ExecutionErr {
-    Type(TypeErr),
-    NoTransition,
-    UnsatisfiedGuard,
-    UndefinedEffect,
-}
-
-pub struct Execution {
-    current_location: Location,
-    vars: Eval,
-    pg: Rc<ProgramGraph>,
-}
-
-impl Execution {
-    pub fn new(initial_location: Location, pg: Rc<ProgramGraph>) -> Self {
-        Execution {
-            current_location: initial_location,
-            vars: Eval::new(),
-            pg,
+    fn typecheck(&self, formula: &Formula) -> Result<(), PgError> {
+        match formula {
+            Formula::Not(subf) => self.typecheck(subf),
+            Formula::And(lhs, rhs) | Formula::Or(lhs, rhs) | Formula::Implies(lhs, rhs) => {
+                self.typecheck(lhs).and_then(|()| self.typecheck(rhs))
+            }
+            Formula::Prop(Var(idx)) => {
+                let var = self
+                    .vars
+                    .get(*idx)
+                    .ok_or(PgError::NonExistingVar(Var(*idx)))?;
+                if matches!(var, VarType::Boolean) {
+                    Ok(())
+                } else {
+                    Err(PgError::Mismatched)
+                }
+            }
+            Formula::Equal(lhs, rhs) | Formula::Less(lhs, rhs) | Formula::LessEq(lhs, rhs) => self
+                .typecheck_expr(lhs)
+                .and_then(|()| self.typecheck_expr(rhs)),
+            _ => Ok(()),
         }
     }
 
-    pub fn possible_transitions(&self) -> Result<Vec<(Action, Location)>, TypeErr> {
-        self.pg
-            .transitions
+    fn typecheck_expr(&self, expr: &IntExpr) -> Result<(), PgError> {
+        match expr {
+            IntExpr::Const(_) => Ok(()),
+            IntExpr::Var(Var(idx)) => {
+                let var = self
+                    .vars
+                    .get(*idx)
+                    .ok_or(PgError::NonExistingVar(Var(*idx)))?;
+                if matches!(var, VarType::Integer) {
+                    Ok(())
+                } else {
+                    Err(PgError::Mismatched)
+                }
+            }
+            IntExpr::Opposite(expr) => self.typecheck_expr(expr),
+            IntExpr::Sum(lhs, rhs) | IntExpr::Mult(lhs, rhs) => self
+                .typecheck_expr(lhs)
+                .and_then(|()| self.typecheck_expr(rhs)),
+        }
+    }
+
+    pub fn build(mut self) -> ProgramGraph {
+        // Since vectors of effects and transitions will become unmutable,
+        // they should be shrunk to take as little space as possible
+        self.effects.iter_mut().for_each(Vec::shrink_to_fit);
+        self.effects.shrink_to_fit();
+        self.transitions.shrink_to_fit();
+        // Vars are not going to be unmutable,
+        // but their number will be constant anyway
+        self.vars.shrink_to_fit();
+        // Build program graph
+        ProgramGraph {
+            current_location: Self::INITIAL_LOCATION,
+            vars: self
+                .vars
+                .iter()
+                .map(|var_type| match var_type {
+                    VarType::Boolean => Val::Boolean(false),
+                    VarType::Integer => Val::Integer(0),
+                })
+                .collect(),
+            effects: Rc::new(self.effects),
+            transitions: Rc::new(self.transitions),
+        }
+    }
+
+    pub fn initial_location(&self) -> Location {
+        Self::INITIAL_LOCATION
+    }
+}
+
+pub struct ProgramGraph {
+    current_location: Location,
+    vars: Vec<Val>,
+    effects: Rc<Vec<Vec<(Var, Effect)>>>,
+    transitions: Rc<Vec<HashMap<(Action, Location), Formula>>>,
+}
+
+impl ProgramGraph {
+    pub fn possible_transitions(&self) -> Vec<(Action, Location)> {
+        self.transitions
             .get(self.current_location.0)
             .unwrap_or(&HashMap::new())
             .iter()
-            .filter_map(|((action, post), guard)| match guard.eval(&self.vars) {
-                Err(err) => Some(Err(err)),
-                Ok(check) if check => Some(Ok((*action, *post))),
-                _ => None,
+            .filter_map(|((action, post), guard)| {
+                if self.eval_formula(guard) {
+                    Some((*action, *post))
+                } else {
+                    None
+                }
             })
-            .collect::<Result<Vec<_>, _>>()
+            .collect::<Vec<_>>()
     }
 
-    pub fn transition(&mut self, action: Action, post_state: Location) -> Result<(), ExecutionErr> {
+    pub fn transition(&mut self, action: Action, post_state: Location) -> Result<(), PgError> {
         let guard = self
-            .pg
             .transitions
             .get(self.current_location.0)
-            .ok_or(ExecutionErr::NoTransition)?
+            .expect("location must exist")
             .get(&(action, post_state))
-            .ok_or(ExecutionErr::NoTransition)?;
-        if guard.eval(&self.vars).map_err(ExecutionErr::Type)? {
-            let effect = self
-                .pg
+            .ok_or(PgError::NoTransition)?;
+        if self.eval_formula(guard) {
+            for (var, effect) in self
                 .effects
                 .get(action.0)
-                .ok_or(ExecutionErr::UndefinedEffect)?;
-            effect(&mut self.vars);
+                .expect("action has been validated before")
+            {
+                // Not using the 'Self::assign' method because:
+                // - borrow checker
+                // - effects are validated before, so no need to type-check again
+                *self
+                    .vars
+                    .get_mut(var.0)
+                    .expect("effect has been validated before") = self.eval(effect);
+            }
             self.current_location = post_state;
             Ok(())
         } else {
-            Err(ExecutionErr::UnsatisfiedGuard)
+            Err(PgError::UnsatisfiedGuard)
         }
+    }
+
+    pub(super) fn eval(&self, effect: &Effect) -> Val {
+        match effect {
+            Effect::Bool(formula) => Val::Boolean(self.eval_formula(formula)),
+            Effect::Integer(expr) => Val::Integer(self.eval_expr(expr)),
+        }
+    }
+
+    pub(super) fn assign(&mut self, var: Var, val: Val) -> Result<Val, PgError> {
+        let var_content = self
+            .vars
+            .get_mut(var.0)
+            .ok_or(PgError::NonExistingVar(var))?;
+        match var_content {
+            Val::Boolean(_) if !matches!(val, Val::Boolean(_)) => Err(PgError::Mismatched),
+            Val::Integer(_) if !matches!(val, Val::Integer(_)) => Err(PgError::Mismatched),
+            _ => {
+                let previous_val = *var_content;
+                *var_content = val;
+                Ok(previous_val)
+            }
+        }
+    }
+
+    fn eval_formula(&self, formula: &Formula) -> bool {
+        match formula {
+            Formula::And(lhs, rhs) => self.eval_formula(lhs) && self.eval_formula(rhs),
+            Formula::Or(lhs, rhs) => self.eval_formula(lhs) || self.eval_formula(rhs),
+            Formula::Implies(lhs, rhs) => !self.eval_formula(lhs) || self.eval_formula(rhs),
+            Formula::Not(subform) => !self.eval_formula(subform),
+            Formula::Prop(var) => {
+                let val = self
+                    .vars
+                    .get(var.0)
+                    .expect("formula has been validated before");
+                if let Val::Boolean(prop) = val {
+                    *prop
+                } else {
+                    unreachable!("formula has been validated before");
+                }
+            }
+            Formula::Equal(lhs, rhs) => self.eval_expr(lhs) == self.eval_expr(rhs),
+            Formula::Less(lhs, rhs) => self.eval_expr(lhs) < self.eval_expr(rhs),
+            Formula::LessEq(lhs, rhs) => self.eval_expr(lhs) <= self.eval_expr(rhs),
+            Formula::True => true,
+            Formula::False => false,
+        }
+    }
+
+    fn eval_expr(&self, expr: &IntExpr) -> Integer {
+        match expr {
+            IntExpr::Const(int) => *int,
+            IntExpr::Var(var) => {
+                if let Val::Integer(val) = self
+                    .vars
+                    .get(var.0)
+                    .expect("formula has been validated before")
+                {
+                    *val
+                } else {
+                    unreachable!("formula has been validated before");
+                }
+            }
+            IntExpr::Opposite(expr) => -self.eval_expr(expr),
+            IntExpr::Sum(lhs, rhs) => self.eval_expr(lhs) + self.eval_expr(rhs),
+            IntExpr::Mult(lhs, rhs) => self.eval_expr(lhs) * self.eval_expr(rhs),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn program_graph() -> Result<(), PgError> {
+        // Create Program Graph
+        let mut builder = ProgramGraphBuilder::new();
+        // Variables
+        let battery = builder.new_var(VarType::Integer);
+        // Locations
+        let initial = builder.initial_location();
+        let left = builder.new_location();
+        let center = builder.new_location();
+        let right = builder.new_location();
+        // Actions
+        let initialize = builder.new_action();
+        builder.add_effect(initialize, battery, Effect::Integer(IntExpr::Const(3)))?;
+        let move_left = builder.new_action();
+        builder.add_effect(
+            move_left,
+            battery,
+            Effect::Integer(IntExpr::Sum(
+                Box::new(IntExpr::Var(battery)),
+                Box::new(IntExpr::Opposite(Box::new(IntExpr::Const(1)))),
+            )),
+        )?;
+        let move_right = builder.new_action();
+        builder.add_effect(
+            move_right,
+            battery,
+            Effect::Integer(IntExpr::Sum(
+                Box::new(IntExpr::Var(battery)),
+                Box::new(IntExpr::Opposite(Box::new(IntExpr::Const(1)))),
+            )),
+        )?;
+        // Guards
+        let out_of_charge = Formula::Less(IntExpr::Const(0), IntExpr::Var(battery));
+        // Program graph definition
+        builder.add_transition(initial, initialize, center, Formula::True)?;
+        builder.add_transition(left, move_right, center, out_of_charge.clone())?;
+        builder.add_transition(center, move_right, right, out_of_charge.clone())?;
+        builder.add_transition(right, move_left, center, out_of_charge.clone())?;
+        builder.add_transition(center, move_left, left, out_of_charge)?;
+        // Execution
+        let mut pg = builder.build();
+        assert_eq!(pg.possible_transitions().len(), 1);
+        pg.transition(initialize, center)?;
+        assert_eq!(pg.possible_transitions().len(), 2);
+        pg.transition(move_right, right)?;
+        assert_eq!(pg.possible_transitions().len(), 1);
+        pg.transition(move_right, right).expect_err("already right");
+        assert_eq!(pg.possible_transitions().len(), 1);
+        pg.transition(move_left, center)?;
+        assert_eq!(pg.possible_transitions().len(), 2);
+        pg.transition(move_left, left)?;
+        assert_eq!(pg.possible_transitions().len(), 0);
+        pg.transition(move_left, left).expect_err("battery = 0");
+        Ok(())
     }
 }
