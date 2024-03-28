85b0b96b6eb0a490c9234121dc4879c7dcbb122d
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2024-03-28 15:34:42 -0400

    Build executable content for `on_exit` and `transition`, plus some necessary code refactoring.
    Signed-off-by Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- src/cs_builder.rs
+++ src/cs_builder.rs
@@ -1,11 +1,11 @@
 use std::{collections::HashMap, env::current_dir};
 
 use anyhow::Ok;
-use log::info;
+use log::{info, trace};
 
 use crate::{
-    parser::*, Channel, ChannelSystem, ChannelSystemBuilder, CsExpr, CsFormula, CsIntExpr, Integer,
-    PgId, VarType,
+    parser::*, Channel, ChannelSystem, ChannelSystemBuilder, CsExpr, CsFormula, CsIntExpr,
+    CsLocation, Integer, PgId, VarType,
 };
 
 #[derive(Debug)]
@@ -62,6 +62,7 @@ impl Sc2CsVisitor {
 
     // TODO: Optimize CS by removing unnecessary states
     fn build_fsm(&mut self, fsm: &Fsm) -> anyhow::Result<()> {
+        trace!("build fsm {}", fsm.id);
         // Initialize fsm.
         let pg_id = self.fsm_ids.get(&fsm.id).cloned().unwrap_or_else(|| {
             let pg_id = self.cs.new_program_graph();
@@ -74,8 +75,6 @@ impl Sc2CsVisitor {
         // Conventionally, the entry-point for a state is a location associated to the id of the state.
         // In particular, the id of the initial state of the fsm has to correspond to the initial location of the program graph.
         states.insert(fsm.initial.to_owned(), initial);
-        let true_cond = CsFormula::new_true(pg_id);
-
         // Var representing the current event
         let current_event = self.cs.new_var(pg_id, VarType::Integer)?;
         // Implement internal queue
@@ -96,99 +95,44 @@ impl Sc2CsVisitor {
         let next_transition = self.cs.new_action(pg_id)?;
 
         // Consider each of the fsm's states
-        for (state_name, state) in fsm.states.iter() {
+        for (_, state) in fsm.states.iter() {
+            trace!("build state {}", state.id);
             // Each state is modeled by multiple locations connected by transitions
-            // Starting location
-            let start_loc = if let Some(start_loc) = states.get(state_name) {
+            // A starting location is used as a point-of-entry to the execution of the state.
+            let start_loc = if let Some(start_loc) = states.get(&state.id) {
                 *start_loc
             } else {
                 let start_loc = self.cs.new_location(pg_id)?;
-                states.insert(state_name.to_owned(), start_loc);
+                states.insert(state.id.to_owned(), start_loc);
                 start_loc
             };
             // Execute the state's `onentry` executable content
             let mut onentry_loc = start_loc;
             for executable in state.on_entry.iter() {
-                match executable {
-                    Executable::Raise { event } => {
-                        let event_idx = self.events.get(event).cloned().unwrap_or_else(|| {
-                            let event_idx = self.events.len() as Integer;
-                            self.events.insert(event.to_owned(), event_idx);
-                            event_idx
-                        });
-                        let raise = self.cs.new_communication(
-                            pg_id,
-                            int_queue,
-                            crate::Message::Send(CsExpr::from_expr(CsIntExpr::new_const(
-                                pg_id, event_idx,
-                            ))),
-                        )?;
-                        let next_loc = self.cs.new_location(pg_id)?;
-                        // queue the internal event
-                        self.cs.add_transition(
-                            pg_id,
-                            onentry_loc,
-                            raise,
-                            next_loc,
-                            true_cond.to_owned(),
-                        )?;
-                        onentry_loc = next_loc;
-                    }
-                    Executable::Send { event, target } => {
-                        let target_id = self.fsm_ids.get(target).cloned().unwrap_or_else(|| {
-                            let pg_id = self.cs.new_program_graph();
-                            self.fsm_ids.insert(target.to_owned(), pg_id);
-                            pg_id
-                        });
-                        let event_id = self.events.get(event).cloned().unwrap_or_else(|| {
-                            let event_id = self.events.len() as Integer;
-                            self.events.insert(event.to_owned(), event_id);
-                            event_id
-                        });
-                        let target_ext_queue = self
-                            .external_queues
-                            .get(target)
-                            .cloned()
-                            .unwrap_or_else(|| self.cs.new_channel(VarType::Integer, None));
-                        let send = self.cs.new_communication(
-                            pg_id,
-                            target_ext_queue,
-                            crate::Message::Send(CsExpr::from_expr(CsIntExpr::new_const(
-                                target_id, event_id,
-                            ))),
-                        )?;
-                        let next_loc = self.cs.new_location(pg_id)?;
-                        // queue the internal event
-                        self.cs.add_transition(
-                            pg_id,
-                            onentry_loc,
-                            send,
-                            next_loc,
-                            true_cond.to_owned(),
-                        )?;
-                        onentry_loc = next_loc;
-                    }
-                }
+                // Each executable content attaches suitable transitions to the point-of-entry location
+                // and returns the target of such transitions as updated point-of-entry location.
+                onentry_loc = self.add_executable(executable, pg_id, int_queue, onentry_loc)?;
             }
 
-            // location where eventless transitions activate
+            // Location where eventless/NULL transitions activate
             let mut null_trans = onentry_loc;
-            // location where transitions with named event from internal queue activate
+            // Location where internal events are dequeued
             let int_queue_loc = self.cs.new_location(pg_id)?;
-            // location where transitions with named event from external queue activate
+            // Location where external events are dequeued
             let ext_queue_loc = self.cs.new_location(pg_id)?;
-            let mut named_trans = self.cs.new_location(pg_id)?;
-            // dequeue a new internal event and search for first active named transition
+            // Location where eventful transitions activate
+            let mut eventful_trans = self.cs.new_location(pg_id)?;
+            // Transition dequeueing a new internal event and searching for first active eventful transition
             self.cs.add_transition(
                 pg_id,
                 int_queue_loc,
                 dequeue_int,
-                named_trans,
-                true_cond.to_owned(),
+                eventful_trans,
+                CsFormula::new_true(pg_id),
             )?;
-            // check if internal queue is empty;
-            // if so, move to external queue
-            // notice that one and only one of int_dequeue and empty_int_queue can be executed at a given time
+            // Action denoting checking if internal queue is empty;
+            // if so, move to external queue.
+            // Notice that one and only one of `int_dequeue` and `empty_int_queue` can be executed at a given time.
             let empty_int_queue =
                 self.cs
                     .new_communication(pg_id, int_queue, crate::Message::ProbeEmptyQueue)?;
@@ -197,20 +141,21 @@ impl Sc2CsVisitor {
                 int_queue_loc,
                 empty_int_queue,
                 ext_queue_loc,
-                true_cond.to_owned(),
+                CsFormula::new_true(pg_id),
             )?;
-            // dequeue a new external event and search for first active named transition
+            // Dequeue a new external event and search for first active named transition.
             self.cs.add_transition(
                 pg_id,
                 ext_queue_loc,
                 dequeue_ext,
-                named_trans,
-                true_cond.to_owned(),
+                eventful_trans,
+                CsFormula::new_true(pg_id),
             )?;
 
-            // consider each of the state's transitions
+            // Consider each of the state's transitions.
             for transition in state.transitions.iter() {
-                // get or create the location corresponding to the target state
+                trace!("build transition {transition:#?}");
+                // Get or create the location corresponding to the target state.
                 let target_loc = if let Some(target_loc) = states.get(&transition.target) {
                     *target_loc
                 } else {
@@ -219,17 +164,22 @@ impl Sc2CsVisitor {
                     target_loc
                 };
 
-                // action correponding to executing the transition
+                // Location corresponding to checking if the transition is active.
+                // Has to be defined depending on the type of transition.
+                let check_trans_loc;
+                // Action correponding to executing the transition.
                 let exec_transition = self.cs.new_action(pg_id)?;
-                // location corresponding to the original state after verifying the transition is not active
+                // Location corresponding to verifying the transition is not active and moving to next one.
                 let next_trans_loc = self.cs.new_location(pg_id)?;
+                // Guard for transition.
+                // Has to be defined depending on the type of transition, etc...
+                let guard;
                 // TODO: implement guards
-                let guard = CsFormula::new_true(pg_id);
-                let not_guard = CsFormula::negation(guard.to_owned());
                 // TODO: add effects
 
+                // Proceed on whether the transition is eventless or activated by event.
                 if let Some(event) = &transition.event {
-                    // named event transition
+                    // Retreive or assign a unique id to the event.
                     let event_id = if let Some(&event_id) = self.events.get(event) {
                         event_id
                     } else {
@@ -237,69 +187,138 @@ impl Sc2CsVisitor {
                         self.events.insert(event.to_owned(), event_id);
                         event_id
                     };
-                    // check if the current event (internal or external) corresponds to the event activating the transition
-                    let guard = CsFormula::and(
-                        guard,
-                        CsFormula::eq(
-                            CsIntExpr::new_var(current_event),
-                            CsIntExpr::new_const(pg_id, event_id),
-                        )?,
+                    // Check if the current event (internal or external) corresponds to the event activating the transition.
+                    guard = CsFormula::eq(
+                        CsIntExpr::new_var(current_event),
+                        CsIntExpr::new_const(pg_id, event_id),
                     )?;
-                    let not_guard = CsFormula::negation(guard.to_owned());
-                    self.cs.add_transition(
-                        pg_id,
-                        named_trans,
-                        exec_transition,
-                        target_loc,
-                        guard.to_owned(),
-                    )?;
-                    self.cs.add_transition(
-                        pg_id,
-                        named_trans,
-                        next_transition,
-                        next_trans_loc,
-                        not_guard,
-                    )?;
-                    named_trans = next_trans_loc;
+                    // Check this transition after the other eventful transitions.
+                    check_trans_loc = eventful_trans;
+                    // Move location of next eventful transitions to a new location.
+                    eventful_trans = next_trans_loc;
                 } else {
-                    // NULL (unnamed) event transition
-                    self.cs.add_transition(
-                        pg_id,
-                        null_trans,
-                        exec_transition,
-                        target_loc,
-                        guard.to_owned(),
-                    )?;
-                    self.cs.add_transition(
-                        pg_id,
-                        null_trans,
-                        next_transition,
-                        next_trans_loc,
-                        not_guard,
-                    )?;
+                    // // NULL (unnamed) event transition
+                    // No event needs to happen in order to trigger this transition.
+                    guard = CsFormula::new_true(pg_id);
+                    // Check this transition after the other eventless transitions.
+                    check_trans_loc = null_trans;
+                    // Move location of next eventless transitions to a new location.
                     null_trans = next_trans_loc;
                 }
+
+                // If transition is active, execute the relevant executable content and then the transition to the target.
+                {
+                    let mut exec_trans_loc = self.cs.new_location(pg_id)?;
+                    self.cs.add_transition(
+                        pg_id,
+                        check_trans_loc,
+                        exec_transition,
+                        exec_trans_loc,
+                        guard.to_owned(),
+                    )?;
+                    // First execute the executable content of the state's `on_exit` tag,
+                    // then that of the `transition` tag.
+                    for exec in state.on_exit.iter().chain(transition.effects.iter()) {
+                        exec_trans_loc =
+                            self.add_executable(exec, pg_id, int_queue, exec_trans_loc)?;
+                    }
+                    // Transitioning to the target state/location.
+                    // At this point, the transition cannot be stopped so there can be no guard.
+                    self.cs.add_transition(
+                        pg_id,
+                        exec_trans_loc,
+                        exec_transition,
+                        target_loc,
+                        CsFormula::new_true(pg_id),
+                    )?;
+                }
+                // If the current transition is not actice, move on to check the next one.
+                let not_guard = CsFormula::negation(guard.to_owned());
+                self.cs.add_transition(
+                    pg_id,
+                    check_trans_loc,
+                    next_transition,
+                    next_trans_loc,
+                    not_guard,
+                )?;
             }
 
-            // connect NULL events with named events
-            // by transitioning from last "NUll" location to dequeuing event location
+            // Connect NULL events with named events
+            // by transitioning from last "NUll" location to dequeuing event location.
             self.cs.add_transition(
                 pg_id,
                 null_trans,
                 next_transition,
                 int_queue_loc,
-                true_cond.to_owned(),
+                CsFormula::new_true(pg_id).to_owned(),
             )?;
-            // return to dequeue a new (internal or external) event
+            // Return to dequeue a new (internal or external) event.
             self.cs.add_transition(
                 pg_id,
-                named_trans,
+                eventful_trans,
                 next_transition,
                 int_queue_loc,
-                true_cond.to_owned(),
+                CsFormula::new_true(pg_id),
             )?;
         }
         Ok(())
+    }
+
+    fn add_executable(
+        &mut self,
+        executable: &Executable,
+        pg_id: PgId,
+        int_queue: Channel,
+        loc: CsLocation,
+    ) -> Result<CsLocation, anyhow::Error> {
+        match executable {
+            Executable::Raise { event } => {
+                let event_idx = self.events.get(event).cloned().unwrap_or_else(|| {
+                    let event_idx = self.events.len() as Integer;
+                    self.events.insert(event.to_owned(), event_idx);
+                    event_idx
+                });
+                let raise = self.cs.new_communication(
+                    pg_id,
+                    int_queue,
+                    crate::Message::Send(CsExpr::from_expr(CsIntExpr::new_const(pg_id, event_idx))),
+                )?;
+                let next_loc = self.cs.new_location(pg_id)?;
+                // queue the internal event
+                self.cs
+                    .add_transition(pg_id, loc, raise, next_loc, CsFormula::new_true(pg_id))?;
+                Ok(next_loc)
+            }
+            Executable::Send { event, target } => {
+                let target_id = self.fsm_ids.get(target).cloned().unwrap_or_else(|| {
+                    let pg_id = self.cs.new_program_graph();
+                    self.fsm_ids.insert(target.to_owned(), pg_id);
+                    pg_id
+                });
+                let event_id = self.events.get(event).cloned().unwrap_or_else(|| {
+                    let event_id = self.events.len() as Integer;
+                    self.events.insert(event.to_owned(), event_id);
+                    event_id
+                });
+                let target_ext_queue = self
+                    .external_queues
+                    .get(target)
+                    .cloned()
+                    .unwrap_or_else(|| self.cs.new_channel(VarType::Integer, None));
+                let send = self.cs.new_communication(
+                    pg_id,
+                    target_ext_queue,
+                    crate::Message::Send(CsExpr::from_expr(CsIntExpr::new_const(
+                        target_id, event_id,
+                    ))),
+                )?;
+                let next_loc = self.cs.new_location(pg_id)?;
+                // queue the internal event
+                self.cs
+                    .add_transition(pg_id, loc, send, next_loc, CsFormula::new_true(pg_id))?;
+                Ok(next_loc)
+            }
+        }
     }
 
     fn build(self) -> CsModel {
