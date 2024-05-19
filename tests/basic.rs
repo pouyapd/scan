81932bbc2d872c02295555b975ddbeaa56d8b80d
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2024-05-19 12:20:04 +0200

    Cleanup tests and assets folders
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- tests/basic.rs
+++ tests/basic.rs
@@ -18,6 +18,7 @@ fn test(file: PathBuf) -> anyhow::Result<()> {
     let parser = Parser::parse(file)?;
     let mut model = parser.build_model();
     let mut steps = 0;
+    assert!(!model.possible_transitions().is_empty());
     while let Some((pg_id, act, loc)) = model.possible_transitions().first().cloned() {
         model.transition(pg_id, act, loc)?;
         steps += 1;
