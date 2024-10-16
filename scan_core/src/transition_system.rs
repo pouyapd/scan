cc99ab891b3a9b1c0dc6e4a26355357ff9b9933c
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2024-10-17 01:22:29 +0200

    Add progress bar
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- scan_core/src/transition_system.rs
+++ scan_core/src/transition_system.rs
@@ -4,8 +4,8 @@ use itertools::Itertools;
 use log::info;
 use rand::prelude::*;
 use rayon::prelude::*;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::Arc;
 
 /// Trait implementing a Transition System (TS), as defined in [^1].
 /// As such, it is possible to verify it against MTL specifications.
@@ -165,8 +165,7 @@ pub trait TransitionSystem: Clone + Send + Sync {
                     // If some assume is not satisfied,
                     // disregard state and move on.
                     return Continue((s, f));
-                } else if guarantees.iter().all(|p| p.eval(trace.as_slice())) && rng.gen_bool(0.666)
-                {
+                } else if guarantees.iter().all(|p| p.eval(trace.as_slice())) {
                     // If all guarantees are satisfied,
                     s += 1;
                 } else {
@@ -190,10 +189,10 @@ pub trait TransitionSystem: Clone + Send + Sync {
         assumes: &[Mtl<usize>],
         confidence: f64,
         precision: f64,
-    ) -> f64 {
+        s: Arc<AtomicU32>,
+        f: Arc<AtomicU32>,
+    ) {
         // WARN FIXME TODO: Implement algorithm for 2.4 Distributed sample generation in Budde et al.
-        let s = AtomicUsize::new(0);
-        let f = AtomicUsize::new(0);
         (0..usize::MAX)
             .into_par_iter()
             .filter_map(|_| {
@@ -201,29 +200,24 @@ pub trait TransitionSystem: Clone + Send + Sync {
                 self.clone().experiment(guarantees, assumes, &mut rng)
             })
             .inspect(|result| {
-                if *result {
+                if *result && rand::thread_rng().gen_bool(0.666) {
                     // If all guarantees are satisfied,
-                    let s = s
-                        .fetch_update(Relaxed, Relaxed, |s| s.checked_add(1))
-                        .expect("");
+                    let mut s = s.fetch_add(1, Ordering::Relaxed);
+                    s += 1;
                     info!("runs: {s} successes");
                 } else {
                     // If guarantee is violated, we have found a counter-example!
-                    let f = f
-                        .fetch_update(Relaxed, Relaxed, |f| f.checked_add(1))
-                        .expect("");
+                    let mut f = f.fetch_add(1, Ordering::Relaxed);
+                    f += 1;
                     info!("runs: {f} failures");
                 }
             })
             .take_any_while(|_| {
-                let s = s.load(Relaxed) as u32;
-                let f = f.load(Relaxed) as u32;
+                let s = s.load(Ordering::Relaxed);
+                let f = f.load(Ordering::Relaxed);
                 adaptive_bound(s, f, confidence, precision) > (s + f) as f64
             })
             .count();
-        let s = s.into_inner();
-        let f = f.into_inner();
-        s as f64 / (s + f) as f64
     }
 }
 
@@ -231,13 +225,23 @@ pub trait TransitionSystem: Clone + Send + Sync {
 // Carlos E. Budde, Pedro R. D’Argenio, Arnd Hartmanns, Sean Sedwards.
 // International Journal on Software Tools for Technology Transfer (2020) 22:759–780
 // https://doi.org/10.1007/s10009-020-00563-2
-fn okamoto_bound(confidence: f64, precision: f64) -> f64 {
+pub fn okamoto_bound(confidence: f64, precision: f64) -> f64 {
     (2f64 / (1f64 - confidence)).ln() / (2f64 * precision.powf(2f64))
 }
 
-fn adaptive_bound(s: u32, f: u32, confidence: f64, precision: f64) -> f64 {
+pub fn adaptive_bound(s: u32, f: u32, confidence: f64, precision: f64) -> f64 {
     let n = s + f;
-    let avg = s as f64 / n as f64;
+    let avg = s as f64 / n.max(1) as f64;
     4f64 * okamoto_bound(confidence, precision)
         * (0.25f64 - ((avg - 0.5f64).abs() - (2f64 * precision / 3f64)).powf(2f64))
+}
+
+pub fn derive_precision(s: u32, f: u32, confidence: f64) -> f64 {
+    let n = s + f;
+    let avg = s as f64 / n as f64;
+    let k = 2f64 * (2f64 / (1f64 - confidence)).ln();
+    let a = (n as f64) + (4f64 * k / 9f64);
+    let b = -4f64 * k / 3f64 * ((avg - 0.5f64).abs());
+    let c = k * ((avg - 0.5f64).powf(2f64) - 0.25f64);
+    (-b + (b.powf(2f64) - 4f64 * a * c).sqrt()) / (2f64 * a)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use clap::Parser as ClapParser;
+use indicatif::{ProgressBar, ProgressStyle};
 use scan::*;
 use scan_fmt_xml::scan_core::*;
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc,
+    },
+};
 
 /// A statistical model checker for large concurrent systems
 #[derive(ClapParser)]
@@ -47,14 +54,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("No counter-example found");
         }
     } else {
-        println!("Verifying model with confidence={confidence}, precision={precision}");
-        let rate = scxml_model.model.par_adaptive(
-            &scxml_model.guarantees,
-            &scxml_model.assumes,
-            confidence,
-            precision,
+        println!(
+            "Verifying model '{}' with confidence={confidence} and precision={precision}",
+            cli.model
+                .file_stem()
+                .and_then(|s| s.to_str())
+                .unwrap_or("???")
         );
-        println!("Success rate: {rate}");
+        let s = Arc::new(AtomicU32::new(0));
+        let f = Arc::new(AtomicU32::new(0));
+        let mag = (-precision.log10().floor()) as usize;
+        std::thread::scope(|scope| {
+            let s0 = Arc::clone(&s);
+            let f0 = Arc::clone(&f);
+            scope.spawn(move || {
+                scxml_model.model.par_adaptive(
+                    &scxml_model.guarantees,
+                    &scxml_model.assumes,
+                    confidence,
+                    precision,
+                    s0,
+                    f0,
+                );
+            });
+            let s1 = Arc::clone(&s);
+            let f1 = Arc::clone(&f);
+            scope.spawn(move || {
+                let mut local_s = 0;
+                let mut local_f = 0;
+                let mut adaptive = adaptive_bound(local_s, local_f, confidence, precision);
+                let style = ProgressStyle::with_template(
+                    "[{elapsed_precise}] {percent:>2}% {wide_bar} {msg} ETA: {eta:<5}",
+                )
+                .unwrap();
+                let bar = ProgressBar::new(adaptive.ceil() as u64).with_style(style);
+                while adaptive > (local_s + local_f) as f64 {
+                    let rate = local_s as f64 / (local_s + local_f) as f64;
+                    let derived_precision = derive_precision(local_s, local_f, confidence);
+                    bar.set_length(adaptive.ceil() as u64);
+                    bar.set_position((local_s + local_f) as u64);
+                    bar.set_message(format!("Rate: {rate:.0$}±{1:.0$}", mag, derived_precision));
+                    std::thread::sleep(std::time::Duration::from_millis(100));
+                    local_s = s1.load(Ordering::Relaxed);
+                    local_f = f1.load(Ordering::Relaxed);
+                    adaptive = adaptive_bound(local_s, local_f, confidence, precision);
+                }
+                bar.finish_and_clear();
+            });
+        });
+        let local_s = s.load(Ordering::Relaxed);
+        let local_f = f.load(Ordering::Relaxed);
+        let rate = local_s as f64 / (local_s + local_f) as f64;
+        println!("Success rate: {rate:.0$}", mag);
     }
     Ok(())
 }
