b2bbe25a3d8a82c63cd778d75deeeea88a5715e5
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2024-11-14 10:30:43 +0100

    Print compressed csv traces
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,7 @@
-use channel_system::EventType;
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use scan::*;
-use scan_fmt_xml::{scan_core::*, ScxmlModel};
+use scan_fmt_xml::scan_core::*;
 use std::{
     path::PathBuf,
     sync::{
@@ -24,91 +23,17 @@ pub struct Cli {
     /// Precision or half-width parameter
     #[arg(short, long, default_value = "0.01")]
     precision: f64,
-    // /// Search for a counterexample
-    // #[arg(long, default_value = "false")]
-    // counterexample: bool,
-    /// Print execution trace
-    #[arg(long, default_value = "false")]
+    /// Print execution traces
+    #[arg(long = "print-traces", default_value = "false")]
     trace: bool,
     /// Max length of execution trace before it is stopped
-    #[arg(long, default_value = "10000")]
+    #[arg(long, default_value = "1000000")]
     length: usize,
 }
 
 impl Cli {
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         let scxml_model = scan_fmt_xml::load(&self.model)?;
-        if self.trace {
-            self.trace(scxml_model)
-        } else {
-            self.verify(scxml_model)
-        }
-    }
-
-    // fn _counterexample(&self, scxml_model: ScxmlModel) -> Result<(), Box<dyn std::error::Error>> {
-    //     println!(
-    //         "Searching for counterexample with confidence={}, precision={}",
-    //         self.confidence, self.precision
-    //     );
-    //     if let Some(trace) = scxml_model.model.find_counterexample(
-    //         &scxml_model.guarantees,
-    //         &scxml_model.assumes,
-    //         self.confidence,
-    //         self.precision,
-    //     ) {
-    //         println!("Counterexample trace:");
-    //         scan::print_state(&scxml_model, scxml_model.model.labels());
-    //         for (event, state) in trace {
-    //             if matches!(event.event_type, EventType::Receive(_) | EventType::Send(_)) {
-    //                 print_event(&scxml_model, event);
-    //                 print_state(&scxml_model, state);
-    //             }
-    //         }
-    //     } else {
-    //         println!("No counter-example found");
-    //     }
-    //     Ok(())
-    // }
-
-    fn trace(&self, scxml_model: ScxmlModel) -> Result<(), Box<dyn std::error::Error>> {
-        println!("Execution trace");
-        let (trace, result) = scxml_model.model.clone().example(
-            Vec::from_iter(
-                scxml_model
-                    .guarantees
-                    .iter()
-                    .cloned()
-                    .map(StateValuationVector::new),
-            ),
-            Vec::from_iter(
-                scxml_model
-                    .assumes
-                    .iter()
-                    .cloned()
-                    .map(StateValuationVector::new),
-            ),
-            self.length,
-        );
-        scan::print_state(&scxml_model, scxml_model.model.labels());
-        for (time, event, state) in trace {
-            if matches!(event.event_type, EventType::Receive(_) | EventType::Send(_)) {
-                print_event(&scxml_model, event, time);
-                print_state(&scxml_model, state);
-            }
-        }
-        if let Some(result) = result {
-            if result {
-                println!("Execution successful");
-            } else {
-                println!("Execution unsuccessful");
-            }
-        } else {
-            println!("Execution inconclusive");
-        }
-        Ok(())
-    }
-
-    fn verify(&self, scxml_model: ScxmlModel) -> Result<(), Box<dyn std::error::Error>> {
         let model_name = self
             .model
             .file_stem()
@@ -122,17 +47,26 @@ impl Cli {
         let bar_s = s.to_owned();
         let bar_f = f.to_owned();
         std::thread::spawn(move || print_progress_bar(bar_s, bar_f, confidence, precision));
+        if self.trace {
+            std::fs::remove_dir_all("./traces").ok();
+            std::fs::create_dir("./traces").expect("create traces dir");
+            std::fs::create_dir("./traces/success").expect("create success dir");
+            std::fs::create_dir("./traces/failure").expect("create failure dir");
+            std::fs::create_dir("./traces/undetermined").expect("create undetermined dir");
+        }
         scxml_model.model.par_adaptive(
             &Vec::from_iter(
                 scxml_model
                     .guarantees
-                    .into_iter()
+                    .iter()
+                    .cloned()
                     .map(StateValuationVector::new),
             ),
             &Vec::from_iter(
                 scxml_model
                     .assumes
-                    .into_iter()
+                    .iter()
+                    .cloned()
                     .map(StateValuationVector::new),
             ),
             confidence,
@@ -140,6 +74,7 @@ impl Cli {
             self.length,
             s.to_owned(),
             f.to_owned(),
+            self.trace.then_some(PrintTrace::new(&scxml_model)),
         );
         let s = s.load(Ordering::Relaxed);
         let f = f.load(Ordering::Relaxed);
@@ -170,6 +105,7 @@ fn print_progress_bar(s: Arc<AtomicU32>, f: Arc<AtomicU32>, confidence: f64, pre
     .unwrap()
     .progress_chars(FINE_BAR);
     let bar = ProgressBar::new(bound.ceil() as u64).with_style(style);
+    bar.set_position(0);
     // Magnitude of precision, to round results to sensible number of digits
     let mag = precision.log10().abs().ceil() as usize;
     while bound > (local_s + local_f) as f64 {
@@ -185,8 +121,8 @@ fn print_progress_bar(s: Arc<AtomicU32>, f: Arc<AtomicU32>, confidence: f64, pre
                 "Success rate: {avg:.0$}±{derived_precision:.0$}",
                 mag
             ));
-            bar.tick();
         }
+        bar.tick();
         // Sleep a while to limit update/refresh rate.
         std::thread::sleep(std::time::Duration::from_millis(32));
         local_s = s.load(Ordering::Relaxed);
