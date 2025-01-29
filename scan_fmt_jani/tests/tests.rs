9a393db52730cf3ce5cb46671badbbdfde86a697
Federico Solinas <122788246+keb77@users.noreply.github.com>
2025-01-29 21:57:55 +0100

    Modified test function for jani
    Signed-off-by: keb77 <federicosolinas78@gmail.com>

--- scan_fmt_jani/tests/tests.rs
+++ scan_fmt_jani/tests/tests.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use rand::seq::IteratorRandom;
 
 const MAXSTEP: usize = 1000;
 
@@ -24,18 +25,31 @@ fn jani_test2() {
 //     test(Path::new("./tests/dining_crypt3.jani"))
 // }
 
+// fn test(path: &Path) {
+//     let ast = scan_fmt_jani::Parser::parse(path).unwrap();
+//     let mut model = scan_fmt_jani::ModelBuilder::build(ast).unwrap();
+//     let mut steps = 0;
+//     assert!(model.possible_transitions().count() > 0);
+//     while let Some((pg_id, act, loc)) = model
+//         .possible_transitions()
+//         .take(1)
+//         .collect::<Vec<_>>()
+//         .pop()
+//     {
+//         model.transition(pg_id, act, loc).unwrap();
+//         steps += 1;
+//         assert!(steps < MAXSTEP, "step limit reached");
+//     }
+// }
+
 fn test(path: &Path) {
     let ast = scan_fmt_jani::Parser::parse(path).unwrap();
     let mut model = scan_fmt_jani::ModelBuilder::build(ast).unwrap();
     let mut steps = 0;
     assert!(model.possible_transitions().count() > 0);
-    while let Some((pg_id, act, loc)) = model
-        .possible_transitions()
-        .take(1)
-        .collect::<Vec<_>>()
-        .pop()
-    {
-        model.transition(pg_id, act, loc).unwrap();
+    let mut rng = rand::thread_rng();
+    while let Some((pg_id, action, destination)) = model.possible_transitions().choose(&mut rng) {
+        model.transition(pg_id, action, destination).unwrap();
         steps += 1;
         assert!(steps < MAXSTEP, "step limit reached");
     }
