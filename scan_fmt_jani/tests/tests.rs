013e38e9af8094b1d4eea2e7c832fdb6294405d4
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2025-02-07 19:51:34 +0100

    Fix `rand` deprecation warning
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- scan_fmt_jani/tests/tests.rs
+++ scan_fmt_jani/tests/tests.rs
@@ -25,36 +25,15 @@ fn jani_test2() {
 //     test(Path::new("./tests/dining_crypt3.jani"))
 // }
 
-// fn test(path: &Path) {
-//     let ast = scan_fmt_jani::Parser::parse(path).unwrap();
-//     let mut model = scan_fmt_jani::ModelBuilder::build(ast).unwrap();
-//     let mut steps = 0;
-//     assert!(model.possible_transitions().count() > 0);
-//     while let Some((pg_id, act, loc)) = model
-//         .possible_transitions()
-//         .take(1)
-//         .collect::<Vec<_>>()
-//         .pop()
-//     {
-//         model.transition(pg_id, act, loc).unwrap();
-//         steps += 1;
-//         assert!(steps < MAXSTEP, "step limit reached");
-//     }
-// }
-
 fn test(path: &Path) {
     let ast = scan_fmt_jani::Parser::parse(path).unwrap();
     let mut model = scan_fmt_jani::ModelBuilder::build(ast).unwrap();
     let mut steps = 0;
     assert!(model.possible_transitions().count() > 0);
-    let mut rng = rand::thread_rng();
-    // let mut trans = 0;
+    let mut rng = rand::rng();
     while let Some((pg_id, action, destination)) = model.possible_transitions().choose(&mut rng) {
-        // trans += 1;
-        // println!("TRS #{trans:05}: {pg_id:?} transition by {action:?} to {destination:?}");
         model.transition(pg_id, action, destination).unwrap();
         steps += 1;
         assert!(steps < MAXSTEP, "step limit reached");
     }
-    // println!("{model:?}");
 }
